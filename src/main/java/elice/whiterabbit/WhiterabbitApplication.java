4d913c143b13deb235c49a27c0f549bf012f6808
[SW6] 장보근 <elicespritte@gmail.com>
2024-06-18 22:04:27 +0900

    first commit

---
+++ src/main/java/elice/whiterabbit/WhiterabbitApplication.java
@@ -0,0 +1,13 @@
+package elice.whiterabbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiterabbitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhiterabbitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/elice/whiterabbit/WhiterabbitApplicationTests.java
@@ -0,0 +1,13 @@
+package elice.whiterabbit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhiterabbitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
